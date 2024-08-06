f480550334d24c1611b9006833bc12ae06eb8ef5
dtorannpu <dtorannpu@gmail.com>
2024-08-06 13:02:48 +0900

    Lambda Powertoolsを追加してロギングを追加

--- aws_lambda_sample_python/lambda_function.py
+++ aws_lambda_sample_python/lambda_function.py
@@ -1,4 +1,13 @@
-def handler(event, context):
+from aws_lambda_powertools import Logger
+from aws_lambda_powertools.utilities.typing import LambdaContext
+
+logger = Logger()
+
+
+@logger.inject_lambda_context
+def handler(event: dict, context: LambdaContext) -> dict:
+    logger.info("test");
+    logger.info("hoge");
     return {
         "statusCode": 200,
         "body": "hello world",
--- tests/lambda_function_test.py
+++ tests/lambda_function_test.py
@@ -1,11 +1,26 @@
+import pytest
+from dataclasses import dataclass
 from aws_lambda_sample_python.lambda_function import handler
 
 
-def test_lambda_handler():
+@pytest.fixture
+def lambda_context():
+    @dataclass
+    class LambdaContext:
+        function_name: str = "test"
+        memory_limit_in_mb: int = 128
+        invoked_function_arn: str = (
+            "arn:aws:lambda:ap-northeast-1:000000000:function:test"
+        )
+        aws_request_id: str = "52fdfc07-2182-154f-163f-5f0f9a621d72"
+
+    return LambdaContext()
+
+
+def test_lambda_handler(lambda_context):
     event = {}
-    context = {}
 
-    response = handler(event, context)
+    response = handler(event, lambda_context)
 
-    assert response['statusCode'] == 200
-    assert response['body'] == 'hello world'
+    assert response["statusCode"] == 200
+    assert response["body"] == "hello world"
